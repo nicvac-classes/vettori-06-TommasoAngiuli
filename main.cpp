0d8967334d4cab468a43cc2a4934ad89ae3b20f5
TommasoAngiuli <tommaso.angiuli@marconibari.edu.it>
2025-02-26 21:44:10 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main() 
+{
+    int n, i, imin, tempimin;
+    do {
+        cout << "inserisci la dimensione del vettore" << endl;
+        cin >> n;
+    } while (n <= 0);
+    string nomi[n], nazioni[n];
+    double tempi[n];
+    i = 0;
+    while (i <= n - 1) {
+        cout << "inserisci nome, nazionalità e tempo del " << i + 1 << " atleta" << endl;
+        cin >> nomi[i];
+        cin >> nazioni[i];
+        cin >> tempi[i];
+        i = i + 1;
+    }
+    i = 0;
+    while (i <= n - 1) {
+        cout << "il " << i + 1 << "° atleta si chiama " << nomi[i] << " ha nazionalità " << nazioni[i] << " e tempo " << tempi[i] << endl;
+        i = i + 1;
+    }
+    imin = 0;
+    tempimin = (int) tempi[0];
+    i = 0;
+    while (i <= n - 1) {
+        if (tempi[i] < tempimin) {
+            imin = i;
+            tempimin = (int) tempi[i];
+        }
+        i = i + 1;
+    }
+    cout << "l'atleta vincitore è " << nomi[imin] << " ha nazionalità " << nazioni[imin] << " e tempo " << tempi[imin] << endl;
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
